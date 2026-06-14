5b206429abf211ec87665899e987a58185fd874d
farah-romdhane <farahromdhaane@gmail.com>
2026-06-13 22:07:33 -0400

    detection dependances intre feuille

--- backend/models/workbook.py
+++ backend/models/workbook.py
@@ -5,9 +5,16 @@ class Workbook:
         self.file_path = file_path
 
         self.worksheets = []
+        self.dependencies = []
 
     def add_worksheet(self, worksheet):
         self.worksheets.append(worksheet)
 
     def get_worksheets(self):
         return self.worksheets
+    
+    def add_dependency(self, dependency):
+        self.dependencies.append(dependency)
+
+    def get_dependencies(self):
+        return self.dependencies
